96724541bc7ca5ce9d0578b0f429d12e8ee88f44
Moises Narvaez <moisesnarvaez@gmail.com>
2018-03-21 22:39:00 -0500

    Add support for ; separator

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
 require 'sinatra'
 require 'csv'
 
+def process_csv(params, separator)
+  result = {}
+  CSV.foreach(params[:file][:tempfile], { :col_sep => separator }) do |row|
+    result[row[0]] = (result[row[0]] || 0) + row[1].to_f
+  end
+  result
+end
+
 get '/' do
   erb :index
 end
 
 post '/' do
-  result = {}
-
-  CSV.foreach(params[:file][:tempfile]) do |row|
-    result[row[0]] = (result[row[0]] || 0) + row[1].to_f
+  begin
+    result = process_csv(params, ',')
+  rescue
+    result = process_csv(params, ';')
   end
 
   content_type 'application/csv'
