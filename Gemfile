aff68d341cc35e6c2582705f55e23045f0a7932c
Moises Narvaez <moisesnarvaez@gmail.com>
2018-03-21 22:15:23 -0500

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+gem 'sinatra'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'csv'
+require 'better_errors'
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  result = {}
+
+  CSV.foreach(params[:file][:tempfile]) do |row|
+    result[row[0]] = (result[row[0]] || 0) + row[1].to_f
+  end
+
+  content_type 'application/csv'
+  attachment   'data.csv'
+
+  CSV.generate do |csv|
+    result.map do |k, v|
+      lb = (v / 1000) * 2.20462
+      csv << [k, lb.ceil]
+    end
+  end
+end
